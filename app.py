3f5b016b21df794273f37a98dc4d64d138c34819
Wilker Gandra <wilkerbruno@users.noreply.github.com>
2025-09-07 23:44:52 -0300

    app
    app.py

--- app.py
+++ app.py
@@ -355,5 +355,7 @@ def internal_error(error):
     }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
     
